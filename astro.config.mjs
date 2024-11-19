dddbd48f5a478ce09d425602fab9ea83d198436b
Charlotte <charlottewiltshire27@outlook.com>
2024-11-19 21:18:30 +0200

    feat: configure Astro with Starlight for Redot documentation

---
+++ astro.config.mjs
@@ -0,0 +1,48 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://docs.experimental.redotengine.org/',
+	integrations: [
+		starlight({
+			title: 'Redot Experimental Documentation',
+			favicon: '/favicon.ico',
+			editLink: {
+				baseUrl: 'https://github.com/Redot-Experimental/redot-experimental-docs/edit/master/src/content/docs/',
+			},
+			social: {
+				github: 'https://github.com/Redot-Engine/redot-engine',
+				discord: "https://discord.gg/redot",
+				'x.com': 'https://x.com/Redot_Engine',
+			},
+			logo: {
+				light: './src/assets/logo.svg',
+				dark: './src/assets/logo.svg',
+				replacesTitle: true,
+			},
+			customCss: [
+				'./src/styles/inter.css',
+				'./src/styles/redot.css',
+			],
+			plugins: [
+				starlightOpenAPI([
+					{
+						base: 'api',
+						label: 'Redot API',
+						schema: './public/openapi.yaml',
+					},
+				]),
+			],
+			sidebar: [
+				{
+					label: 'Contributing',
+					autogenerate: { directory: 'contributing' },
+				},
+				...openAPISidebarGroups,
+			],
+		}),
+	],
+});
